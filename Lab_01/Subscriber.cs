c492e7df6a3ef9ba7e536619c17fb03e928a2c9d
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-09-25 21:44:34 +0300

    Added tariff information and properties
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- Lab_01/Subscriber.cs
+++ Lab_01/Subscriber.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Lab 01, variant 5 - Mobile subscriber information
+// Ivan Hudzinskyi, Alexandr Korotetskyi, Regina Miroshnichenko
+// KIUKI-17-4
+
 namespace Lab_01
 {
     class Subscriber
     {
+        public enum TariffName { Standard, Premium, Premium_plus, Ultimate };
+
+        static private Dictionary<TariffName, float> tariffMinuteCost = new Dictionary<TariffName, float>()
+        {
+            [TariffName.Standard] = 5.0f,
+            [TariffName.Premium] = 2.0f,
+            [TariffName.Premium_plus] = 1.0f,
+            [TariffName.Ultimate] = 0.0f
+        };
+
+        private TariffName currentTariff;
+
+        public string CurrentTariff { get { return currentTariff.ToString().Replace('_', ' '); } }
+
+        public float MinuteTalkCost { get { return tariffMinuteCost[currentTariff]; } }
     }
 }
