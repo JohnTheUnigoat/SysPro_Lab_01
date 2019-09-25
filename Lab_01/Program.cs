24ac3ae1192eaf4ab0e2623b1feffb46741b8746
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-09-25 19:39:50 +0300

    Initial commit
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ Lab_01/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Lab 01, variant 5 - Mobile subscriber information
+// Ivan Hudzinskyi, Alexandr Korotetskyi, Regina Miroshnichenko
+// KIUKI-17-4
+
+namespace Lab_01
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
