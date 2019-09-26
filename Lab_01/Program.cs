7e5f360d3731d4291b279cf920b40807c7c2b1a9
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-09-26 15:57:20 +0300

    Added properties test
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- Lab_01/Program.cs
+++ Lab_01/Program.cs
@@ -12,6 +12,22 @@ namespace Lab_01
 {
     class Program
     {
+        static void PrintSubscriber(Subscriber subscriber)
+        {
+            Console.WriteLine("Phone number: " + subscriber.PhoneNumber);
+            Console.WriteLine("Tariff: " + subscriber.CurrentTariff);
+            Console.WriteLine("Cost per minute: " + subscriber.MinuteTalkCost);
+            Console.WriteLine("Balance: " + subscriber.Balance);
+
+            Console.WriteLine("Call history:");
+            string[] callHistory = subscriber.CallHistory;
+            foreach (var item in callHistory)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+
         static void Main()
         {
             Subscriber subscriber1;
@@ -51,6 +67,21 @@ namespace Lab_01
                 Console.WriteLine(e.GetType() + " " + e.Message);
                 Console.WriteLine();
             }
+
+            Console.WriteLine();
+
+            // Instantiating objects that pass the test
+            subscriber1 = new Subscriber();
+            subscriber2 = new Subscriber("0669539811", Subscriber.TariffName.Premium);
+
+            // Properties test
+            Console.WriteLine("Subscriber 1:");
+            PrintSubscriber(subscriber1);
+
+            Console.WriteLine("Subscriber 2:");
+            PrintSubscriber(subscriber2);
+
+            Console.WriteLine();
         }
     }
 }
