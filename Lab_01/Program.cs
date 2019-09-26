c201dd76fef9b98f24118707a7241904f20152df
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-09-26 15:30:36 +0300

    Added constructor test
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- Lab_01/Program.cs
+++ Lab_01/Program.cs
@@ -14,6 +14,43 @@ namespace Lab_01
     {
         static void Main()
         {
+            Subscriber subscriber1;
+            Subscriber subscriber2;
+            Subscriber subscriber3;
+            Subscriber subscriber4;
+
+            // Constructor test
+            try { subscriber1 = new Subscriber(); }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("Error creating subscriber1!");
+                Console.WriteLine(e.GetType() + " " + e.Message);
+                Console.WriteLine();
+            }
+
+            try { subscriber2 = new Subscriber("0669539811", Subscriber.TariffName.Premium); }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error creating subscriber2!");
+                Console.WriteLine(e.GetType() + " " + e.Message);
+                Console.WriteLine();
+            }
+
+            try { subscriber3 = new Subscriber("798436"); }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error creating subscriber3!");
+                Console.WriteLine(e.GetType() + " " + e.Message);
+                Console.WriteLine();
+            }
+
+            try { subscriber4 = new Subscriber("06695398q1"); }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Error creating subscriber4!");
+                Console.WriteLine(e.GetType() + " " + e.Message);
+                Console.WriteLine();
+            }
         }
     }
 }
